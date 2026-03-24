89f8148e8897c01befc4b31fdfe9d6b0c5ccc00d
Mongxin <1393907820@qq.com>
2026-03-24 14:53:05 +0800

    feat(framework): 完成对基础框架，通用方法的实现

---
+++ framework/src/main/java/com/mongxin/livestart/framework/config/IdempotentConfiguration.java
@@ -0,0 +1,29 @@
+package com.mongxin.livestart.framework.config;
+
+import com.mongxin.livestart.framework.idempotent.NoDuplicateSubmitAspect;
+import com.mongxin.livestart.framework.idempotent.NoMQDuplicateConsumeAspect;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * 幂等组件相关配置类
+ */
+public class IdempotentConfiguration {
+
+    /**
+     * 防止用户重复提交表单信息切面控制器
+     */
+    @Bean
+    public NoDuplicateSubmitAspect noDuplicateSubmitAspect(RedissonClient redissonClient) {
+        return new NoDuplicateSubmitAspect(redissonClient);
+    }
+
+    /**
+     * 防止消息队列消费者重复消费消息切面控制器
+     */
+    @Bean
+    public NoMQDuplicateConsumeAspect noMQDuplicateConsumeAspect(StringRedisTemplate stringRedisTemplate) {
+        return new NoMQDuplicateConsumeAspect(stringRedisTemplate);
+    }
+}
