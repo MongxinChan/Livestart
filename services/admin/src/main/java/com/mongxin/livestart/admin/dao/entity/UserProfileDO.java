46f505bcaba532ff2eb83cab65711dc74468edca
Mongxin <1393907820@qq.com>
2026-03-29 12:04:13 +0800

    feat(admin): 基于社交属性，对User进行分表拆分成两个表

---
+++ services/admin/src/main/java/com/mongxin/livestart/admin/dao/entity/UserProfileDO.java
@@ -0,0 +1,44 @@
+package com.mongxin.livestart.admin.dao.entity;
+
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName("t_user_profile")
+public class UserProfileDO {
+    /**
+     * 关联用户ID (对应 t_user.id)
+     * 注意：此处不使用自动生成，由业务层在注册时手动设置
+     */
+    @TableId
+    private Long userId;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+    /**
+     * 用户头像URL
+     */
+    private String avatar;
+
+    /**
+     * 性别 0:保密 1:男 2:女
+     */
+    private Integer gender;
+
+    /**
+     * 个性签名
+     */
+    private String signature;
+
+    /**
+     * 生日
+     */
+    private Date birthday;
+}
